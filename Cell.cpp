9df9a3b6ab78b50a88a66e3b0bf5e65417c4ccfb
Jack Frankland <30410009+FranklandJack@users.noreply.github.com>
2017-10-07 18:12:00 +0100

    corrected a missing comma in the Cell.cpp file

--- Cell.cpp
+++ Cell.cpp
@@ -1,6 +1,6 @@
 #include "Cell.hpp"
 
-Cell::Cell(int numberPred, int numberPrey bool dry):m_nPredator(numberPred),m_nPrey(numberPrey),m_dry(dry){}
+Cell::Cell(int numberPred, int numberPrey, bool dry):m_nPredator(numberPred),m_nPrey(numberPrey),m_dry(dry){}
 Cell::~Cell(){}
 
 int Cell::getNumbPred(){return m_nPredator;}
