805ac09c80b723733e3099124a81f17de38b445a
dbankova <32613525+dbankova@users.noreply.github.com>
2017-10-20 21:27:37 +0100

    Update main.cpp
    Added a loop creating output files every T steps

--- source/main.cpp
+++ source/main.cpp
@@ -54,14 +54,34 @@ int main(int argc, char const *argv[])
 
      int t=500;
      int num_iterations=int(t/deltaT);
-    
-    for(int i = 1; i <= num_iterations; i++)
-    {
+     int step_average=10;
+     char outputfile[50];
+	
+     for(int iter = 1; iter <= num_iterations; iter++)
+     {
         grid = updateGrid(grid,r,a,b,m,k,l,deltaT);
-        if(0 == i%10)
+        if(0 == iter % step_average)
             std::cout<<grid.predDensity()<< ' ' << grid.preyDensity()<<std::endl;
-       // if(0 == i%T)
+        if(0 == iter % T)
          // print output
+	{
+	//file directory has to be changed
+          sprintf(outputfile,"D:\\output_densities%d.txt",iter/T);
+		
+	    ofstream output_den(outputfile, ios::out);
+        if (output_den.is_open())
+         {
+           for(int i=1; i<=Nrows; i++){
+             for(int j=1; j<=Ncolumns; j++){
+	 //we have to change h[i][j] and p[i][j] for every cell
+                output_den << i <<" "<< j <<" "<< h[i][j] <<" "<< p[i][j] <<endl;
+             }
+            }
+                output_den.close();
+         }
+        else
+                cout<<"Output file could not open!"<<endl;
+	}
 
     }
     
