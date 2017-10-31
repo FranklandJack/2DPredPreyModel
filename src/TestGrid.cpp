ecf2ec86e6408d68fa06a9caa43a1ebe2fcf7fbc
dbankova <32613525+dbankova@users.noreply.github.com>
2017-10-31 23:07:56 +0000

    Update TestGrid.cpp

--- src/TestGrid.cpp
+++ src/TestGrid.cpp
@@ -1,18 +1,26 @@
 #include "TestGrid.hpp"
+const double precision=0.0001;
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestGrid);
 
-/*
+
 void 
 TestGrid::testConstructor()
 {
-	const int **data;
-  	const int numberColumns = 2000;
-	const int numberRows = 1900;
-	Grid NewGrid(numberColumns,numberRows,**data);
-	CPPUNIT_ASSERT_EQUAL( numberColumns, NewGrid.getColumns() );
-    CPPUNIT_ASSERT_EQUAL( numberRows, NewGrid.getRows() );
-	
+        const int landStates [2][3]={{0,1,1},{0,1,1}};
+        const int **inputStates = &landStates;
+        const int numberColumns = 2;
+        const int numberRows = 3;
+        Grid NewGrid(numberColumns,numberRows,**inputStates);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL( numberColumns, NewGrid.getColumns(), precision);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL( numberRows, NewGrid.getRows(), precision);
+        //Is there an easy way to check the cell states of NewGird?
+/*      for(int i=0; i<=1; ++i){
+              for(int j=0; j<=2; ++j){
+                 CPPUNIT_ASSERT_EQUAL( landStates[i][j], NewGrid.?());
+              }
+        }
+  */
   }
 
-*/
+
