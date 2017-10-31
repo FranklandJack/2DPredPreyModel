425d35ffef0ae26b3779ee8bb528051093d53cd8
Jack Frankland <30410009+FranklandJack@users.noreply.github.com>
2017-10-31 09:53:09 +0000

    Tried to fix bug in test

--- src/TestCell.cpp
+++ src/TestCell.cpp
@@ -52,7 +52,7 @@ void TestCell::testSetState()
 
 void TestCell::testSetPreyDensity()
 {
-    b->setPreyDensity(5.0);
+    //b->setPreyDensity(5.0);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(5.0, b->getPreyDensity(), 0.0001);
 }
 
