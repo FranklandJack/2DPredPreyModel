7e12beb6c343a95715354946a4ede83438f03550
dbankova <32613525+dbankova@users.noreply.github.com>
2017-10-31 13:48:42 +0000

    Update TestGrid.cpp

--- src/TestGrid.cpp
+++ src/TestGrid.cpp
@@ -3,5 +3,15 @@
 CPPUNIT_TEST_SUITE_REGISTRATION(TestGrid);
 
 
-
+void 
+TestGrid::testConstructor()
+{
+	const int **data;
+  	const int numberColumns = 2000;
+	const int numberRows = 1900;
+	Grid NewGrid(numberColumns,numberRows,**data);
+	CPPUNIT_ASSERT_EQUAL( numberColumns, NewGrid.getColumns() );
+    CPPUNIT_ASSERT_EQUAL( numberRows, NewGrid.getRows() );
+	
+  }
 
