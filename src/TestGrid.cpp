34ab0757a83206096597ccaf4504c7707f11dbcf
Jack Frankland <30410009+FranklandJack@users.noreply.github.com>
2017-10-31 13:39:42 +0000

    added header and and cpp files for the grid tests

---
+++ src/TestGrid.cpp
@@ -0,0 +1,7 @@
+#include "TestGrid.hpp"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestGrid);
+
+
+
+
---
+++ src/TestGrid.hpp
@@ -0,0 +1,48 @@
+#ifndef TestGrid_hpp
+#define TestGrid_hpp
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestFixture.h>
+#include "Grid.hpp"
+/*
+*
+* A test case that is designed to produce
+* example errors and failures
+*
+*/
+class TestGrid : public CPPUNIT_NS::TestFixture
+{
+
+        // Establish the test suit of TestCell
+        CPPUNIT_TEST_SUITE(TestGrid);
+
+        // Add test method testInit
+        CPPUNIT_TEST(testInit);
+
+        
+
+        // Finish the process
+        CPPUNIT_TEST_SUITE_END();
+
+    public:
+
+        // Overide setUp(), init data etc
+        void setUp();
+
+        // Overide tearDown(), free allocated memory,etc
+        void tearDown();
+
+    protected:
+
+        // Test method testInit
+        void testInit();
+
+        
+
+    private:
+
+        // Three instances of Grid for test
+    Cell *a , *b, *c;
+};
+
+#endif /* TestCell_hpp */
