063794e59f04c3463b9b196369099ffed67b66b9
dbankova <32613525+dbankova@users.noreply.github.com>
2017-11-01 18:27:57 +0000

    Update Grid.cpp
    Added getCellState function

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -362,7 +362,10 @@ int Grid::getColumns() const {return m_columns;}
 
 int Grid::getRows()    const {return m_rows;}
 
-
+Cell::State Grid::getCellState(int rowIndex, int columnIndex)
+{
+         return (*this)(rowIndex,columnIndex).getState();
+}
 
 void Grid::setUniformPredDistribution(double upperBound, std::default_random_engine &generator)
 {
