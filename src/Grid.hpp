ac6297c7c341d98f1defbbea0ff5a670bb48f85c
dbankova <32613525+dbankova@users.noreply.github.com>
2017-11-03 01:39:42 +0000

    Update Grid.hpp
    changed printPPM from void to int type

--- src/Grid.hpp
+++ src/Grid.hpp
@@ -431,7 +431,7 @@ class Grid
          *
          */
 
-        void printPPM(std::ofstream &file) const;
+        int printPPM(std::ofstream &file, int maxNumberPPM) const;
 
         /**
          *
